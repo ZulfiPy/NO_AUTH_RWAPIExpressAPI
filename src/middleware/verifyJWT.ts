7c6f6c24da751fdbfbed98d11be9c3c101d0b008
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-04 11:46:20 +0300

    verify jwt middleware

---
+++ src/middleware/verifyJWT.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
+    const cookies = req?.cookies;
+    if (!cookies['next-auth.session-token']) {
+        return res.status(401).json({ "message": "unauthorized - no session token" });
+    }
+    const nextAuthSessionToken: string = cookies['next-auth.session-token'];
+    jwt.verify(
+        nextAuthSessionToken,
+        process.env.NEXTAUTH_SECRET as string,
+        async (error, decoded) => {
+            if (error) {
+                console.log('error occured', error);
+                return res.status(401).json({ "message": "unauthorized - expired/misused/invalid sigranutre" });
+            }
+            console.log('decoded', decoded);
+            next();
+        }
+    )
+}
+
+export default verifyJWT;
