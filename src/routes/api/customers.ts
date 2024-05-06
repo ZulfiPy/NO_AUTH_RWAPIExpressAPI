f2a8f8349dbdcc1aea0e42d3236edbd701c03315
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-06 07:55:06 +0300

    get route configured with id param

--- src/routes/api/customers.ts
+++ src/routes/api/customers.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getCustomers, createCustomer } from "../../controllers/customersController";
+import { getCustomers, getCustomer, createCustomer } from "../../controllers/customersController";
 
 const router = express.Router();
 
 // /api/customers routes
 router.get('/', getCustomers);
+router.get('/:id', getCustomer);
 router.post('/', createCustomer);
 
 export default router;
