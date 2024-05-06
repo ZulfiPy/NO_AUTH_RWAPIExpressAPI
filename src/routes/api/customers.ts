549b1b158eab332e3b5c5b1d9284153a2a6a3b38
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-06 08:53:29 +0300

    delete route configured

--- src/routes/api/customers.ts
+++ src/routes/api/customers.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getCustomers, getCustomer, createCustomer, updateCustomer } from "../../controllers/customersController";
+import { getCustomers, getCustomer, createCustomer, updateCustomer, deleteCustomer } from "../../controllers/customersController";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/', getCustomers);
 router.get('/:id', getCustomer);
 router.post('/', createCustomer);
 router.put('/:id', updateCustomer);
+router.delete('/:id', deleteCustomer)
 
 export default router;
