703b12f6e439782ba0d0f8a672abf9045cc73a3e
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-09 09:22:58 +0300

    create customer controller modified

--- src/controllers/customersController.ts
+++ src/controllers/customersController.ts
@@ -40,7 +40,7 @@ const createCustomer = async (req: Request, res: Response) => {
     const database = process.env.PG_CUSTOMERS_DB as string;
     await pgConn(database);
 
-    const fieldNames = ['firstName', 'lastName', 'isEstonianResident', 'birthDate', 'driverLicenseNumber', 'address', 'phoneNumber', 'email'];
+    const fieldNames = ['firstName', 'lastName', 'birthDate', 'driversLicenseNumber', 'address', 'phoneNumber', 'email'];
 
     if (Object.values(req.body).length === 0) {
         return res.status(400).json({ 'message': 'body of your request is empty' });
@@ -48,16 +48,17 @@ const createCustomer = async (req: Request, res: Response) => {
 
     for (let field of fieldNames) {
         if (!req.body?.[field]) {
+            console.log('problem here', req.body?.[field], field)
             return res.status(400).json({ 'message': `${field} field is empty, please provide it` });
         }
     }
 
-    const { firstName, lastName, isEstonianResident, personalIDNumber, birthDate, driverLicenseNumber, address, phoneNumber, email } = req.body;
+    const { firstName, lastName, isEstonianResident, isikukood, birthDate, driversLicenseNumber, address, phoneNumber, email } = req.body;
 
     try {
         const pool = getPool(database);
 
-        const createdCustomerPool = await pool.query('INSERT INTO customers (first_name, last_name, is_estonian_resident, personal_id_number, birth_date, driver_license_number, address, phone_number, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;', [firstName, lastName, isEstonianResident, personalIDNumber, birthDate, driverLicenseNumber, address, phoneNumber, email]);
+        const createdCustomerPool = await pool.query('INSERT INTO customers (first_name, last_name, is_estonian_resident, personal_id_number, birth_date, driver_license_number, address, phone_number, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;', [firstName, lastName, isEstonianResident, isikukood, birthDate, driversLicenseNumber, address, phoneNumber, email]);
 
         return res.status(200).json({ 'message': 'new customer created' });
     } catch (error) {
