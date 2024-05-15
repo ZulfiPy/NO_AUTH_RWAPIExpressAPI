0a8402b3d209f92d0db788a93eba569c4ba0a9b0
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-05-15 15:16:26 +0300

    create vehicle controller updated

--- src/controllers/vehiclesController.ts
+++ src/controllers/vehiclesController.ts
@@ -11,25 +11,26 @@ const getVehicles = async (req: Request, res: Response) => {
 
         const vehiclesPool = await pool.query('SELECT * FROM vehicles');
 
-        return res.status(200).json({ 'message': vehiclesPool.rowCount === 0 ? 0 : vehiclesPool.rows });
+        return res.status(200).json({ 'vehicles': vehiclesPool.rowCount === 0 ? 0 : vehiclesPool.rows });
     } catch (error) {
         console.log('error occured while reading vehicles, error:', error);
         return res.status(500).json({ 'message': 'something went wrong' });
     }
 }
 
-const getVehicleByPlateNumber = async (req: Request, res: Response) => {
+const getVehicleById = async (req: Request, res: Response) => {
     const database = process.env.PG_VEHICLES_DB as string;
     await pgConn(database);
 
-    const { plateNumber } = req.params;
+    const { id } = req.params;
+    console.log(id)
 
     try {
         const pool = getPool(database);
 
-        const vehiclePool = await pool.query(`SELECT * FROM vehicles WHERE plate_number = ($1);`, [plateNumber]);
+        const vehiclePool = await pool.query(`SELECT * FROM vehicles WHERE id = ($1);`, [id]);
 
-        return res.status(200).json({ 'vehicle': vehiclePool.rowCount === 0 ? 0 : vehiclePool.rows })
+        return res.status(200).json({ 'vehicle': vehiclePool.rowCount === 0 ? 0 : vehiclePool.rows[0] })
     } catch (error) {
         console.log('error occured while reading vehicle, error:', error);
         return res.status(500).json({ 'message': 'something went wrong' });
@@ -44,7 +45,7 @@ const createVehicle = async (req: Request, res: Response) => {
         return res.status(400).json({ 'message': "body of your request is empty" });
     }
 
-    const fieldNames = ['vin_code', 'plate_number', 'brand', 'model', 'year', 'gearbox', 'colour'];
+    const fieldNames = ['vin_code', 'plate_number', 'brand', 'model', 'year', 'gearbox', 'colour', 'fuel_type'];
 
     for (let field of fieldNames) {
         if (!req.body?.[field]) {
@@ -52,14 +53,14 @@ const createVehicle = async (req: Request, res: Response) => {
         }
     }
 
-    const { vin_code, plate_number, brand, model, year, gearbox, colour } = req.body;
+    const { vin_code, plate_number, brand, model, year, gearbox, colour, fuel_type } = req.body;
 
     try {
         const pool = getPool(database);
 
-        const newVehiclePool = await pool.query('INSERT INTO vehicles (vin_code, plate_number, brand, model, year, gearbox, colour) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;', [vin_code, plate_number, brand, model, year, gearbox, colour]);
+        const newVehiclePool = await pool.query('INSERT INTO vehicles (vin_code, plate_number, brand, model, year, gearbox, colour, fuel_type) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;', [vin_code, plate_number, brand, model, year, gearbox, colour, fuel_type]);
 
-        return res.status(200).json({ 'message': newVehiclePool.rowCount === 0 ? 0 : newVehiclePool.rows[0] })
+        return res.status(200).json({ 'vehicle': newVehiclePool.rowCount === 0 ? 0 : newVehiclePool.rows[0] })
     } catch (error) {
         console.log('error occured while creating vehicles, error:', error);
         return res.status(500).json({ 'message': 'something went wrong' });
@@ -90,7 +91,7 @@ const updateVehicle = async (req: Request, res: Response) => {
 
         const updatedVehiclePool = await pool.query('UPDATE vehicles SET vin_code = ($1), plate_number = ($2), brand = ($3), model = ($4), year = ($5), gearbox = ($6), colour = ($7) where id = ($8) RETURNING *;', [vin_code, plate_number, brand, model, year, gearbox, colour, id]);
 
-        return res.status(200).json({ 'message': updatedVehiclePool.rowCount === 0 ? 0 : updatedVehiclePool.rows[0] })
+        return res.status(200).json({ 'vehicle': updatedVehiclePool.rowCount === 0 ? 0 : updatedVehiclePool.rows[0] })
     } catch (error) {
         console.log('error occured while creating vehicles, error:', error);
         return res.status(500).json({ 'message': 'something went wrong' });
@@ -105,7 +106,7 @@ const deleteVehicle = async (req: Request, res: Response) => {
 
     try {
         const pool = getPool(database);
-        
+
         const deletedVehiclePool = await pool.query('DELETE FROM vehicles WHERE id = ($1);', [id])
 
         return res.status(200).json({ 'message': deletedVehiclePool.rowCount === 0 ? 0 : "vehicle deleted" });
@@ -117,7 +118,7 @@ const deleteVehicle = async (req: Request, res: Response) => {
 
 export {
     getVehicles,
-    getVehicleByPlateNumber,
+    getVehicleById,
     createVehicle,
     updateVehicle,
     deleteVehicle
