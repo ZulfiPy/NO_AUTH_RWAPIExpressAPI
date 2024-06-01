03759f5151bbcbb1e4f7dbdc3ae044d6e78215c8
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-06-01 14:02:36 +0300

    all functions of controller modified to work in pair with hosted pg on supabase

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,155 +1,146 @@
 import { Request, Response } from "express";
-import pgConn from "../config/pgDBConn";
-import { getPool } from "../config/pgDBConn";
+import pgConn from "../config/supabasePGDbConn";
+import { getPool } from "../config/supabasePGDbConn";
 import jwt from "jsonwebtoken";
 
 const getTasks = async (req: Request, res: Response) => {
-    const database = process.env.PG_TASKS_DB as string;
+    const database = process.env.SUPABASE_PG_DB as string;
     await pgConn(database);
 
     const decoded = jwt.decode(req?.cookies['next-auth.session-token']);
 
     if (typeof decoded === 'object' && decoded !== null && 'user' in decoded) {
 
-        const userInfo = decoded.user
-        const username = userInfo?.username
+        const userInfo = decoded.user;
+        const username = userInfo?.username;
 
         try {
             const pool = getPool(database);
 
-            const tasks = await pool.query(`SELECT * FROM tasks WHERE createdby = '${username}'`);
+            const tasks = await pool.query('SELECT * FROM tasks WHERE created_by = ($1);', [username]);
 
             return res.status(200).json({ "tasks": tasks.rowCount === 0 ? 0 : tasks.rows });
         } catch (error) {
-            console.log('error occured while reading tasks:', error);
-            return res.status(500).json({ "message": "Something went wrong" });
+            console.log('error occurred while reading tasks:', error);
+            return res.status(500).json(error);
         }
     }
 
-    return res.status(401).json({ 'message': 'provide session token' });
+    return res.status(401).json({ 'message': 'provide session token' })
 }
 
 const getTasksByUsername = async (req: Request, res: Response) => {
-    const database = process.env.PG_TASKS_DB as string;
+    const database = process.env.SUPABASE_PG_DB as string;
     await pgConn(database);
 
     try {
         const pool = getPool(database);
 
-        const tasks = await pool.query(`SELECT * FROM tasks WHERE createdby = '${req.params.username}'`);
+        const { username } = req.params
+        const tasks = await pool.query('SELECT * FROM tasks WHERE created_by = ($1);', [username]);
 
         return res.status(200).json({ "tasks": tasks.rowCount === 0 ? 0 : tasks.rows });
     } catch (error) {
-        console.log('error occured while reading tasks by username:', error);
-        return res.status(500).json({ "message": "Something went wrong" });
+        console.log('error occurred while reading tasks by username:', error);
+        return res.status(500).json(error);
     }
 }
 
-const getOneTaskById = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const database = process.env.PG_TASKS_DB as string;
+const getOneTaskByIdAndUsername = async (req: Request, res: Response) => {
+    const { id, username } = req.params;
+    const database = process.env.SUPABASE_PG_DB as string;
     await pgConn(database);
 
     try {
         const pool = getPool(database);
 
-        const foundOneTask = await pool.query(`SELECT * FROM tasks WHERE tasks.id = ${id}`);
+        const foundOneTask = await pool.query(`SELECT * FROM tasks WHERE id = ($1) AND created_by = ($2)`, [id, username])
 
         if (!foundOneTask.rows || foundOneTask.rows.length === 0) {
-            return res.status(401).json({ "message": `task by id ${id} not found` });
+            return res.status(404).json({ 'message': `task by id ${id} not found` });
         }
 
-        return res.status(200).json(foundOneTask.rows[0])
+        return res.status(200).json(foundOneTask.rows[0]);
     } catch (error) {
-        console.log('error occured while reading one task', error);
-        return res.status(500).json({ "message": "Something went wrong" });
+        console.log('error occurred while reading tasks by username:', error);
+        return res.status(500).json(error);
     }
 }
 
 const createTask = async (req: Request, res: Response) => {
-    const database = process.env.PG_TASKS_DB as string;
+    const database = process.env.SUPABASE_PG_DB as string;
     await pgConn(database);
 
     if (Object.values(req.body).length === 0) {
-        return res.status(400).json({ "message": "body of your request is empty" });
+        return res.status(400).json({ 'message': 'body of your request is empty' });
     }
 
-    const { title, description, priority, status } = req.body;
-    const username = 'ZulfugarA';
+    const { title, description, priority, status, created_by } = req.body;
 
     try {
         const pool = getPool(database);
 
-        const newTask = await pool.query('INSERT INTO tasks (title, description, priority, status, createdby) VALUES ($1, $2, $3, $4, $5);', [title, description, priority, status, username]);
+        const newTask = await pool.query('INSERT INTO tasks (title, description, priority, status, created_by) VALUES ($1, $2, $3, $4, $5);', [title, description, priority, status, created_by]);
 
-        return res.status(200).json({ "message": "new task created" });
+        return res.status(200).json({ 'message': newTask.rowCount === 0 ? 0 : newTask.rowCount });
     } catch (error) {
-        console.log('error occured while reading tasks by username:', error);
-        return res.status(500).json({ "message": "Something went wrong" });
+        console.log('error occurred while creating task:', error);
+        return res.status(500).json(error);
     }
 }
 
-const updateTaskById = async (req: Request, res: Response) => {
-    const database = process.env.PG_TASKS_DB as string;
+const updateTaskByIdAndUsername = async (req: Request, res: Response) => {
+    const database = process.env.SUPABASE_PG_DB as string;
     await pgConn(database);
 
     if (Object.values(req.body).length === 0) {
-        return res.status(400).json({ "message": "body of your request is empty" });
+        return res.status(400).json({ 'message': 'body of your request is empty' });
     }
 
-    const { id, title, description, priority, status } = req.body;
-    const username = 'ZulfugarA';
+    const { id, title, description, priority, status, created_by } = req.body;
 
     try {
         const pool = getPool(database);
 
-        const editedTask = await pool.query('UPDATE tasks SET title = ($1), description = ($2), priority = ($3), status = ($4) WHERE id = ($5) AND createdby = ($6) RETURNING *;', [title, description, priority, status, id, username]);
+        const editedTask = await pool.query('UPDATE tasks SET title = ($1), description = ($2), priority = ($3), status = ($4) WHERE id = ($5) AND created_by = ($6) RETURNING *;', [title, description, priority, status, id, created_by]);
 
         if (editedTask.rowCount === 0) {
-            return res.status(404).json({ "message": "You do not have such task in the database" });
+            return res.status(404).json({ 'message': 'task not found to be updated' });
         }
 
-        return res.status(200).json({ "UPDATED TASK": editedTask.rows[0] });
-
+        return res.status(200).json({ 'UPDATED TASK': editedTask.rows[0] });
     } catch (error) {
-        console.log('error occured while updating task by id:', error);
-        return res.status(500).json({ "message": "Something went wrong" });
+        console.log('error occurred while updating task:', error);
+        return res.status(500).json(error);
     }
 }
 
 const deleteTaskById = async (req: Request, res: Response) => {
-    const database = process.env.PG_TASKS_DB as string;
+    const { id, username } = req.params
+    const database = process.env.SUPABASE_PG_DB as string;
     await pgConn(database);
-
-    if (!req.body?.id) {
-        return res.status(400).json({ "message": "no ID provided for further task delition" });
-    }
-
-    const { id } = req.body;
 
     try {
         const pool = getPool(database);
 
-        const foundTask = await pool.query('SELECT * FROM tasks WHERE id = ($1);', [id]);
+        const taskToDelete = await pool.query('DELETE FROM tasks WHERE id = ($1) AND created_by = ($2);', [id, username])
 
-        if (foundTask.rowCount === 0) {
-            return res.status(404).json({ "message": `task not found the id ${id}` });
+        if (taskToDelete.rowCount === 0) {
+            return res.status(404).json({ 'message': `task by id ${id} not found for furthel deletion` });
         }
 
-        const taskToDelete = await pool.query('DELETE FROM tasks where id = ($1);', [id]);
-
-        return res.status(200).json({ "message": "task deleted" });
+        return res.status(200).json({ 'message': 'task deleted' });
     } catch (error) {
-        console.log('error occured while task deletion:', error);
-        return res.status(500).json({ "message": "Something went wrong" });
+        console.log('error occurred while task deletion:', error);
+        return res.status(500).json(error);
     }
 }
 
 export {
     getTasks,
-    getOneTaskById,
     getTasksByUsername,
+    getOneTaskByIdAndUsername,
     createTask,
-    updateTaskById,
-    deleteTaskById
+    updateTaskByIdAndUsername,
+    deleteTaskById,
 }
