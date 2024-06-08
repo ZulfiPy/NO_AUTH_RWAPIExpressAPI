9f6cd90c345942a71cc009c45374076a7cf427be
ZulfiPy <zulfizupy@gmail.com>
2024-06-08 11:34:20 +0300

    changed index page text to test the change

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server; LOL KAK TAK');
+    res.send('Express + TypeScript Server; RW-Rent API developed by Zulfugar A.');
 });
 
 // built-in middleware for json
