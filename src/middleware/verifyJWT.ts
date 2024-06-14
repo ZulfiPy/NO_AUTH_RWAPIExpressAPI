04e0d2626618acf1a3a835d5b8dc62a474df5bca
ZulfiPy <zulfizupy@gmail.com>
2024-06-14 11:42:49 +0300

    log updated in verifyJWT middleware

--- src/middleware/verifyJWT.ts
+++ src/middleware/verifyJWT.ts
@@ -5,6 +5,7 @@ const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
     const cookies = req?.cookies;
     console.log('here I am with the cookies', cookies)
     if (!cookies['next-auth.session-token']) {
+        console.log('i return 401')
         return res.status(401).json({ "message": "unauthorized - no session token" });
     }
     const nextAuthSessionToken: string = cookies['next-auth.session-token'];
