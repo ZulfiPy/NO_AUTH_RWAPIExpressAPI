58df596c4a2ce1e4c4ba54a0c11f83f698b2fbbf
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-07-13 18:45:29 +0300

    cors options log

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -4,8 +4,10 @@ const corsOptions = {
     origin: (origin: string | undefined, callback: (arg0: Error | null, arg1: boolean | undefined) => void) => {
         if (allowedOrigins.indexOf(origin as string) !== -1 || !origin) {
             callback(null, true);
+            console.log('cors allowed');
         } else {
             callback(new Error('Not allowed by CORS'), false);
+            console.log('cors not allowed');
         }
     },
     optionsSuccessStatus: 200
